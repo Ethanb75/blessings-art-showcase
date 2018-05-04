fb6b6fd5dd42755093541b010c321510a5c9cab1
Ethan Bellora <Belloramail@gmail.com>
2018-05-04 10:09:07 -0400

    added facebook pixel for client

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,6 +42,21 @@ class IndexPage extends Component {
     gtag('js', new Date());
 
     gtag('config', 'UA-118150096-1');
+
+
+
+    /*Facebook Pixel */
+    !function (f, b, e, v, n, t, s) {
+      if (f.fbq) return; n = f.fbq = function () {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      }; if (!f._fbq) f._fbq = n;
+      n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0;
+      t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
+    }(window,
+      document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '<PixelID>');
+    fbq('track', 'PageView');
   }
   render() {
     let ticketLink = 'https://www.eventbrite.com/e/blessings-artist-showcase-tickets-43759043477?utm_campaign=new_event_email&utm_medium=email&utm_source=eb_email&utm_term=viewmyevent_button'
@@ -327,6 +342,9 @@ class IndexPage extends Component {
             Icons by <a href="http://www.freepik.com" style={{ color: 'white' }}>Freepik</a> & <a href="https://smashicons.com/" style={{ color: 'white' }}>Smashicons</a> via <a href="http://www.flaticon.com" style={{ color: 'white' }}>flaticon.com</a>
           </div>
         </div>
+        <noscript><img height="1" width="1" style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=<PixelID>&ev=PageView&noscript=1" />
+        </noscript>
       </div>
     )
   }
